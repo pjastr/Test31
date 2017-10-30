da2c4428033ffca038cc27ea5eacf25f9aefa417
Piotr <@>
2017-10-30 12:45:50 +0100

    pierwszy commit

---
+++ ConsoleApplication29/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication29
+{
+    class Program
+    {
+        static string OpiszTyp()
+        {
+            return "Metoda bez argumentów";
+        }
+        static string OpiszTyp(int x)
+        {
+            return "Liczba całkowita";
+        }
+        static string OpiszTyp(string x)
+        {
+            return "Łańcuch znaków";
+        }
+        static string OpiszTyp(int x, int y)
+        {
+            return "Dwie liczby calkowite";
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OpiszTyp("hhhh"));
+            Console.ReadKey();
+        }
+    }
+}
